62be6d131c466d4f4ca3af6c72448d85d82b87a1
Vahizan <vahizanv@gmail.com>
2019-09-20 16:59:53 +0100

    Adding sentry error tracking to project

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -4,12 +4,19 @@ import 'lazysizes/plugins/rias/ls.rias';
 import 'lazysizes/plugins/bgset/ls.bgset';
 import 'lazysizes';
 import 'lazysizes/plugins/respimg/ls.respimg';
+import * as Sentry from '@sentry/browser';
 
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
+
+// Sentry Error Tracking
+Sentry.init({
+  dsn: 'https://176f27a607024a9ba5b40e16c1f0a18b@sentry.io/1732306',
+  debug: true,
+});
 
 // Common a11y fixes
 focusHash();
